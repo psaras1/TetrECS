97cc637f52c0f625774d566564bade2e19b35b29
cp2n23 <cp2n23@soton.ac.uk>
2024-03-15 13:41:38 +0000

    Added bindable properties score, level, lives and multiplier to the Game class. Added a score method in the game class to update the score after rows/cols are cleared(called within afterPiece)

--- src/main/java/uk/ac/soton/comp1206/scene/ChallengeScene.java
+++ src/main/java/uk/ac/soton/comp1206/scene/ChallengeScene.java
@@ -1,5 +1,9 @@
 package uk.ac.soton.comp1206.scene;
 
+import javafx.beans.binding.Bindings;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,9 +12,7 @@ import uk.ac.soton.comp1206.component.GameBoard;
 import uk.ac.soton.comp1206.game.Game;
 import uk.ac.soton.comp1206.ui.GamePane;
 import uk.ac.soton.comp1206.ui.GameWindow;
-/**
- * testing repo
- */
+
 
 /**
  * The Single Player challenge scene. Holds the UI for the single player challenge mode in the game.
@@ -19,6 +21,11 @@ public class ChallengeScene extends BaseScene {
 
     private static final Logger logger = LogManager.getLogger(MenuScene.class);
     protected Game game;
+    private Label scoreLabel;
+    private Label levelLabel;
+    private Label livesLabel;
+    private Label multiplierLabel;
+    private HBox stats;
 
     /**
      * Create a new Single Player challenge scene
@@ -26,7 +33,17 @@ public class ChallengeScene extends BaseScene {
      */
     public ChallengeScene(GameWindow gameWindow) {
         super(gameWindow);
+        this.scoreLabel = new Label();
+        this.levelLabel = new Label();
+        this.livesLabel = new Label();
+        this.multiplierLabel = new Label();
         logger.info("Creating Challenge Scene");
+    }
+    public void bindProperties(){
+        scoreLabel.textProperty().bind(Bindings.concat("Score: ",game.getScore().asString()));
+        levelLabel.textProperty().bind(Bindings.concat("Level: ",game.getLevel().asString()));
+        livesLabel.textProperty().bind(Bindings.concat("Lives: ",game.getLives().asString()));
+        multiplierLabel.textProperty().bind(Bindings.concat("Multiplier: ",game.getMultiplier().asString()));
     }
 
     /**
@@ -35,10 +52,17 @@ public class ChallengeScene extends BaseScene {
     @Override
     public void build() {
         logger.info("Building " + this.getClass().getName());
-
         setupGame();
+        bindProperties();
+        //To set all text of labels to white, TODO shorten this
+        scoreLabel.setStyle("-fx-text-fill: white;");
+        levelLabel.setStyle("-fx-text-fill: white;");
+        livesLabel.setStyle("-fx-text-fill: white;");
+        multiplierLabel.setStyle("-fx-text-fill: white;");
 
         root = new GamePane(gameWindow.getWidth(),gameWindow.getHeight());
+
+        var mainPane = new BorderPane();
 
         var challengePane = new StackPane();
         challengePane.setMaxWidth(gameWindow.getWidth());
@@ -46,13 +70,19 @@ public class ChallengeScene extends BaseScene {
         challengePane.getStyleClass().add("menu-background");
         root.getChildren().add(challengePane);
 //        Build the User Interface
-        HBox stats = new HBox();
+        var stats = new HBox(135);
+        stats.setAlignment(Pos.CENTER);
+        BorderPane.setMargin(stats, new Insets(10,0,0,0));
+        mainPane.setTop(stats);
+        stats.getChildren().addAll(scoreLabel, levelLabel, livesLabel, multiplierLabel);
 
-        var mainPane = new BorderPane();
-        challengePane.getChildren().add(mainPane);
+
+
 
         var board = new GameBoard(game.getGrid(),gameWindow.getWidth()/2,gameWindow.getWidth()/2);
         mainPane.setCenter(board);
+        challengePane.getChildren().add(mainPane);
+
 
         //Handle block on gameboard grid being clicked
         board.setOnBlockClick(this::blockClicked); //calls blockClicked from GameBoard class
