f0186a1d65e41787f10df27bbbdff9ec889a439f
cp2n23 <cp2n23@soton.ac.uk>
2024-04-12 14:37:48 +0300

    Added music to loading screen, also pressing escape/enter/space skips to the menu

--- src/main/java/uk/ac/soton/comp1206/scene/LoadingScene.java
+++ src/main/java/uk/ac/soton/comp1206/scene/LoadingScene.java
@@ -5,23 +5,37 @@ import javafx.animation.FadeTransition;
 import javafx.animation.SequentialTransition;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.StackPane;
+import javafx.scene.media.MediaPlayer;
 import javafx.util.Duration;
+import uk.ac.soton.comp1206.game.Multimedia;
 import uk.ac.soton.comp1206.ui.GamePane;
 import uk.ac.soton.comp1206.ui.GameWindow;
 
 public class LoadingScene extends BaseScene{
+  private Multimedia loadingMusic = new Multimedia();
   public LoadingScene(GameWindow gameWindow) {
     super(gameWindow);
   }
 
   @Override
   public void initialise() {
-
+    scene.setOnKeyPressed(e->{
+      switch (e.getCode()){
+        case ESCAPE, SPACE, ENTER:
+          loadingMusic.stopBackgroundMusic();
+          gameWindow.startMenu();
+          break;
+        default:
+          break;
+      }
+    });
   }
 
   @Override
   public void build() {
+    loadingMusic.playBackgroundMusic("/sounds/intro.mp3");
     this.root = new GamePane(gameWindow.getWidth(), gameWindow.getHeight());
     StackPane stackPane = new StackPane();
     stackPane.getStyleClass().add("loading");
@@ -33,14 +47,15 @@ public class LoadingScene extends BaseScene{
 
     stackPane.getChildren().add(ecs);
     this.root.getChildren().add(stackPane);
-    FadeTransition fIn = new FadeTransition(new Duration(2000.0), ecs);
+    FadeTransition fIn = new FadeTransition(new Duration(3000.0), ecs);
     fIn.setToValue(1.0);
-    FadeTransition fOut = new FadeTransition(new Duration(2000.0), ecs);
+    FadeTransition fOut = new FadeTransition(new Duration(3000.0), ecs);
     fOut.setToValue(0.0);
 
     SequentialTransition sequence = new SequentialTransition(fIn, fOut);
     sequence.play();
     sequence.setOnFinished(e -> {
+      loadingMusic.stopBackgroundMusic();
       gameWindow.startMenu();
     });
   }
